4c9368c31d5b3587bee0e62dd0341d6ce67dedf7
Artem Bykov <bykovaa94@mail.ru>
2023-04-11 11:38:44 +0300

    test 12.2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,40 @@
-import { Provider, ErrorBoundary } from "@rollbar/react";
-import "react-toastify/dist/ReactToastify.css";
-import React from "react";
-import ErrorPage from "./ErrorPage.js";
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import ErrorPage from './ErrorPage.js';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
   Navigate,
-} from "react-router-dom";
-import { store } from "./slice/index";
-import { Provider as ProviderReduce } from "react-redux";
-import { useSelector } from "react-redux";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
-import { ToastContainer } from "react-toastify";
-import ModalWraper from "./ModalWraper";
-import FormAtorithation from "./FormAtorithation.js";
-import FormRegistration from "./FormRegistration.js";
-import Chat from "./Chat.js";
+} from 'react-router-dom';
+import { store } from './slice/index';
+import { Provider as ProviderReduce } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import { ToastContainer } from 'react-toastify';
+import ModalWraper from './ModalWraper';
+import FormAtorithation from './FormAtorithation.js';
+import FormRegistration from './FormRegistration.js';
+import Chat from './Chat.js';
 
 const PrivateRoute = ({ children }) => {
-  localStorage.getItem("userId");
+  localStorage.getItem('userId');
   const logger = useSelector((state) => state.logger.authLogger);
   const location = useLocation();
 
   return logger ? (
     children
   ) : (
-    <Navigate to="/login" state={{ from: location }} />
+    <Navigate to='/login' state={{ from: location }} />
   );
 };
 
 const rollbarConfig = {
-  accessToken: "c86ec99fa46146649a64bb0835a41ac4",
-  environment: "testenv",
+  accessToken: 'c86ec99fa46146649a64bb0835a41ac4',
+  environment: 'testenv',
 };
 
 const App = () => {
@@ -47,16 +47,16 @@ const App = () => {
             <Router>
               <Routes>
                 <Route
-                  path="/"
+                  path='/'
                   element={
                     <PrivateRoute>
                       <Chat />
                     </PrivateRoute>
                   }
                 />
-                <Route path="/login" element={<FormAtorithation />} />
-                <Route path="/signup" element={<FormRegistration />} />
-                <Route path="*" element={<ErrorPage />} />
+                <Route path='/login' element={<FormAtorithation />} />
+                <Route path='/signup' element={<FormRegistration />} />
+                <Route path='*' element={<ErrorPage />} />
               </Routes>
             </Router>
             <ModalWraper />
